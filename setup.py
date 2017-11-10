fbe3afb9326697c96cd90fc99b8c8fbeaa407ce0
Blaise Thompson <blaise@untzag.com>
2017-11-09 20:08:13 -0600

    skeleton

---
+++ setup.py
@@ -0,0 +1,61 @@
+#! /usr/bin/env python
+
+
+# --- import -------------------------------------------------------------------------------------
+
+
+import os
+from setuptools import setup, find_packages
+
+
+# --- define -------------------------------------------------------------------------------------
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+extra_files = []
+extra_files.append(os.path.join(here, 'LICENSE'))
+extra_files.append(os.path.join(here, 'README.rst'))
+extra_files.append(os.path.join(here, 'requirements.txt'))
+extra_files.append(os.path.join(here, 'VERSION'))
+
+
+# --- functions ----------------------------------------------------------------------------------
+
+
+with open(os.path.join(here, 'requirements.txt')) as f:
+    required = f.read().splitlines()
+
+
+with open(os.path.join(here, 'VERSION')) as version_file:
+    version = version_file.read().strip()
+
+
+# --- setup --------------------------------------------------------------------------------------
+
+
+setup(
+    name='tidy_headers',
+    packages=find_packages(),
+    package_data={'': extra_files},
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest', 'pytest-cov'],
+    install_requires=required,
+    version=version,
+    description='Easy headers, inspired by tidy data formats.',
+    author='Blaise Thompson',
+    author_email='blaise@untzag.com',
+    license='MIT',
+    url='https://github.com/untzag/tidy_headers',
+    keywords='metadata file format',
+    classifiers=['Development Status :: 5 - Production/Stable',
+                 'License :: OSI Approved :: MIT License',
+                 'Natural Language :: English',
+                 'Programming Language :: Python :: 2',
+                 'Programming Language :: Python :: 2.7',
+                 'Programming Language :: Python :: 3',
+                 'Programming Language :: Python :: 3.3',
+                 'Programming Language :: Python :: 3.4',
+                 'Programming Language :: Python :: 3.5']
+)
