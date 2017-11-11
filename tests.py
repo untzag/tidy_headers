0288c0247285281aa7b8585eb7d265f5907a197b
Blaise Thompson <blaise@untzag.com>
2017-11-10 19:00:20 -0600

    tests

---
+++ tests.py
@@ -0,0 +1,69 @@
+# --- import -------------------------------------------------------------------------------------
+
+
+import tempfile
+
+import numpy as np
+
+import tidy_headers
+
+
+# --- helpers ------------------------------------------------------------------------------------
+
+
+def assert_dict_equal(a, b):
+    assert len(a.keys()) == len(b.keys())
+    for key, value in a.items():
+        if hasattr(value, 'dtype'):
+            assert np.asarray(b[key]).all() == np.array(value).all()
+        else:
+            assert b[key] == value
+
+
+# --- test ---------------------------------------------------------------------------------------
+
+
+def test_float():
+    f = tempfile.NamedTemporaryFile()
+    d = {'zero': 0., 'four': 4., 'large': 124., 'negative': -12., 'fraction': 0.5}
+    tidy_headers.write(f.name, d)
+    assert_dict_equal(d, tidy_headers.read(f.name))
+    f.close()
+
+
+def test_int():
+    f = tempfile.NamedTemporaryFile()
+    d = {'zero': 0, 'four': 4, 'large': 1200000, 'negative': -1}
+    tidy_headers.write(f.name, d)
+    assert_dict_equal(d, tidy_headers.read(f.name))
+    f.close()
+
+
+def test_list():
+    f = tempfile.NamedTemporaryFile()
+    d = {}
+    d['hello'] = ['a', 'b', 'c', 'd']
+    d['blaise'] = [0, 1, 2, 3, 4, 5, 6, -124]
+    d['mixture'] = ['blaise', 'wisconsin', 'echo', 'wisconsin']
+    tidy_headers.write(f.name, d)
+    assert_dict_equal(d, tidy_headers.read(f.name))
+    f.close()
+
+
+def test_array():
+    f = tempfile.NamedTemporaryFile()
+    d = {}
+    d['5'] = np.random.random(5)
+    d['12, 45'] = np.random.random((12, 45))
+    d['7, 15, 2'] = np.random.random((7, 15, 2))
+    tidy_headers.write(f.name, d)
+    assert_dict_equal(d, tidy_headers.read(f.name))
+    f.close()
+
+
+def test_str():
+    f = tempfile.NamedTemporaryFile()
+    d = {'blaise': 'thompson', 'red': 'tomato', 'love': 'hate', 'madison': 'wisconsin'}
+    tidy_headers.write(f.name, d)
+    assert_dict_equal(d, tidy_headers.read(f.name))
+    f.close()
--- tidy_headers/__version__.py
+++ tidy_headers/__version__.py
@@ -13,7 +13,7 @@ import os
 here = os.path.abspath(os.path.dirname(__file__))
 
 
-__all__ = ['__version__', '__branch__']
+__all__ = ['__version__']
 
 
 # --- version -------------------------------------------------------------------------------------
@@ -22,14 +22,3 @@ __all__ = ['__version__', '__branch__']
 # read from VERSION file
 with open(os.path.join(os.path.dirname(here), 'VERSION')) as f:
     __version__ = f.read().strip()
-
-
-# add git branch, if appropriate
-p = os.path.join(os.path.dirname(here), '.git', 'HEAD')
-if os.path.isfile(p):
-    with open(p) as f:
-        __branch__ = f.readline().rstrip().split(r'/')[-1]
-    if __branch__ != 'master':
-        __version__ += '-' + __branch__
-else:
-    __branch__ = None
