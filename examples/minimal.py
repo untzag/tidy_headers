b2912ccba724da4615be364593c21ca55eca9694
Blaise Thompson <blaise@untzag.com>
2017-11-10 21:11:42 -0600

    examples

---
+++ examples/minimal.py
@@ -0,0 +1,39 @@
+"""Minimum example of tidy headers."""
+
+
+# --- import -------------------------------------------------------------------------------------
+
+
+import os
+import time
+import collections
+
+import numpy as np
+
+import tidy_headers
+
+
+# --- define -------------------------------------------------------------------------------------
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+# --- workspace ----------------------------------------------------------------------------------
+
+
+filepath = os.path.join(here, 'minimal.txt')
+
+# create metadata
+meta = collections.OrderedDict()
+meta['time'] = time.time()
+meta['name'] = 'Blaise Thompson'
+meta['colors'] = ['red', 'blue', 'green', 'white']
+meta['number'] = 42
+meta['array'] = np.random.random((5, 3))
+
+# write metadata
+tidy_headers.write(filepath, meta)
+
+# read metadata
+print(tidy_headers.read(filepath))
---
+++ examples/named.py
@@ -0,0 +1,40 @@
+"""tidy_headers works great as a way to store column names."""
+
+
+# --- import -------------------------------------------------------------------------------------
+
+
+import os
+import time
+import collections
+
+import numpy as np
+
+import tidy_headers
+
+
+# --- define -------------------------------------------------------------------------------------
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+# --- workspace ----------------------------------------------------------------------------------
+
+
+filepath = os.path.join(here, 'named.txt')
+
+
+# make data
+meta = collections.OrderedDict()
+meta['time'] = time.time()
+meta['name'] = ['index', 'apples', 'oranges']
+index = np.arange(21)
+apples = np.random.randint(0, 99, 21)
+oranges = np.random.randint(0, 99, 21)
+
+
+# write
+tidy_headers.write(filepath, meta)
+with open(filepath, 'ab') as f:
+    np.savetxt(f, np.vstack([index, apples, oranges]).T, fmt='%i', delimiter='\t')
